8a30f3e7a8b2787b2796bdcdca8424c782f218af
Thaíssy Uanna Damian <thais@pc-thaissy.com>
2025-08-12 13:36:41 -0300

    construindo a estrutrura e a seção Sobre mim

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/thaissydamian.png" />
+    <Titulo fontSize={20}>Thaíssy Damian</Titulo>
+  </aside>
+)
+export default Sidebar
